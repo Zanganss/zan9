feb73b17c0f23bdb2af7f429e9368440c22ed9cf
Zanganss <76145765+Zanganss@users.noreply.github.com>
2021-01-23 16:13:33 +0800

    Update listmenu.js

--- src/listmenu.js
+++ src/listmenu.js
@@ -226,9 +226,9 @@ const listmenu = (prefix) => {
 ║
 ╚═ Ketik *${prefix}info* untuk melihat list informasi tentang bot
        Ketik *${prefix}owner* untuk melihat kontak owner
-         Mau donasi? 08165466368(dana)
+         Mau donasi? 085340827717(pulsa)
          Jika tidak ingin donasi bantu Follow Ig aja kak 
-         _instagram.com/maslent11
-    _* ICHI BOT © 2021*_`
+         instagram.com/zanganzz_
+    _* Zan botz © 2021*_`
 }
 exports.listmenu = listmenu
